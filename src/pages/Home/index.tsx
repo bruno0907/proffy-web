0fb26df9258bd3bd58c81610dc6efd305d1bd25c
Bruno <bruno0907@gmail.com>
2020-08-11 02:20:19 -0300

    First commit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
 import studyIcon from '../../assets/images/icons/study.svg'
+import power from '../../assets/images/icons/power.svg'
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 
@@ -22,13 +23,22 @@ function Landing(){
     }, []);
 
     return (
-        <div id="page-lading">
+        <div id="page-lading" className="landing">            
             <div id="page-landing-content" className="container">
+                <div className="home-header">
+                    <Link to="/" className="profile-button">
+                        <img src="https://avatars3.githubusercontent.com/u/54812906?s=460&u=230c6ae207fa7fd5735456ef3011c8771549c8cb&v=4" alt="" className="avatar"/>
+                        <span className="user-name">Bruno Mariani</span>
+                    </Link>
+                    <button className="logout-button"><img src={power} alt="Logout"/></button>
+                </div>
                 <div className="logo-container">
                     <img src={logoImg} alt="Proffy"/>
                     <h2>Sua pataforma de estudos online.</h2>
                 </div>
                 <img src={landingImg} alt="Landing" className="hero-image"/>
+
+                <span className="cta">Seja bem vindo.<br /><b>O que deseja fazer?</b></span>
 
                 <div className="buttons-container">
                     <Link to="/study" className="study">
@@ -44,6 +54,7 @@ function Landing(){
                 <span className="total-connections">
                     Total de: {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
                 </span>
+
             </div>
         </div>
     );
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
-import Landing from './pages/Landing'
+import Home from './pages/Home'
 import TeacherList from './pages/TeacherList'
 import TeacherForm from './pages/TeacherForm'
 
 function Routes(){
     return(
         <BrowserRouter>
-            <Route exact path="/" component={Landing} />
+            <Route exact path="/" component={Home} />
             <Route exact path="/study" component={TeacherList} />
             <Route exact path="/give-classes" component={TeacherForm} />
         </BrowserRouter>
